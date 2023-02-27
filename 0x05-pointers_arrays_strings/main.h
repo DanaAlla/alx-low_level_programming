7e495c1ddd9b9ca6d7655070d341b7100cfc0077
DanaAlla <danaalla27@gmail.com>
2023-02-27 08:39:44 -0800

    update

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void reset_to_98(int n);
+void swap_int(inta, int b);
+int _strlen(chars);
+void _puts(char str);
+void print_rev(chars);
+void rev_string(char s);
+void puts2(charstr);
+void puts_half(char str);
+void print_array(inta, int n);
+char _strcpy(chardest, char src);
+int _atoi(chars);
